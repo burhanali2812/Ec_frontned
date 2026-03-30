4311cf39936920397c8d89f5a1073d5c6a365e8c
burhanali2812 <syedburhanali830@gmail.com>
2026-03-30 23:52:39 +0500

    admin

---
+++ src/components/TeacherPanel.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './Sidebar';
+
+function TeacherPanel() {
+  return (
+    <div>
+        <Sidebar>
+            <h1>Teacher Panel</h1>
+        </Sidebar>
+    </div>
+  )
+}
+
+export default TeacherPanel
