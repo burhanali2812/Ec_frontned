06b05b526cc6883fde7396402b956fc811baa984
burhanali2812 <syedburhanali830@gmail.com>
2026-03-31 00:05:12 +0500

    admin

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import AdminPanel from "./components/AdminPanel";
+import TeacherPanel from "./components/TeacherPanel";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="/adminPanel" element={<AdminPanel />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/adminPanel" element={<AdminPanel />} />
+        <Route path="/teacherPanel" element={<TeacherPanel />} />
+
       {/* Optional: Redirect unknown routes */}
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
